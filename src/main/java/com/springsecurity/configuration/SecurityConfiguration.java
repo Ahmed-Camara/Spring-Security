bfd90ef0f1d4d3be5e5b4e77f437dbfac371047f
Ahmed-Camara <camarahmed123@gmail.com>
2021-03-26 01:53:24 +0600

    InMemory authentication

---
+++ src/main/java/com/springsecurity/configuration/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.springsecurity.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("ahmed").password("ahmed123").roles("EMPLOYEE"))
+			.withUser(users.username("camara").password("camara123").roles("STUDENT"))
+			.withUser(users.username("idriss").password("idriss123").roles("ADMIN"))
+			.withUser(users.username("almam").password("almam123").roles("ADMIN"))
+			.withUser(users.username("ali").password("ali123").roles("MANAGER"));
+		
+	}
+}
