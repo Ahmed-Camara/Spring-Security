234b4df2eec36845a3e1a18bcc25f702d0b458ae
Ahmed Camara <44366946+Ahmed-Camara@users.noreply.github.com>
2021-03-26 23:57:16 +0600

    Database script

---
+++ script.sql
@@ -0,0 +1,20 @@
+INSERT INTO users 
+VALUES 
+('ahmed','{noop}ahmed123',1),
+('camara','{noop}camara123',1),
+('idriss','{noop}idriss123',1),
+('almam','{noop}almam123',1),
+('ali','{noop}ali123',1);
+
+
+INSERT INTO authorities 
+VALUES 
+('ahmed','ROLE_EMPLOYEE'),
+('camara','ROLE_EMPLOYEE'),
+('camara','ROLE_STUDENT'),
+('idriss','ROLE_EMPLOYEE'),
+('idriss','ROLE_ADMIN'),
+('almam','ROLE_EMPLOYEE'),
+('almam','ROLE_ADMIN'),
+('ali','ROLE_EMPLOYEE'),
+('ali','ROLE_MANAGER');
