5407184bdbc23dafe8ee050cddd801887b4001bf
Ahmed Camara <44366946+Ahmed-Camara@users.noreply.github.com>
2021-03-26 23:59:06 +0600

    Database script

--- script.sql
+++ script.sql
@@ -1,3 +1,18 @@
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `authorities_idx_1` (`username`,`authority`),
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 INSERT INTO users 
 VALUES 
 ('ahmed','{noop}ahmed123',1),
